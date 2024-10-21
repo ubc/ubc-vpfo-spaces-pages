823369dba735a79349f3fdacd710eb9f6d713d3c
Kasia Gawlak <kasia@paper-leaf.com>
2024-10-21 11:28:25 -0600

    added Whitney font for use in templates

--- includes/enqueues.php
+++ includes/enqueues.php
@@ -13,5 +13,12 @@ function ubc_vpfo_spaces_pages_enqueue_styles() {
         array(), // Dependencies
         '1.0.0'
     );
+
+    wp_enqueue_style(
+        'font-whitney',
+        plugin_dir_url( __DIR__ ) . 'fonts/whitney/font-whitney.css',
+        array(), // Dependencies
+        '1.0.0'
+    );
 }
 add_action( 'wp_enqueue_scripts', 'ubc_vpfo_spaces_pages_enqueue_styles' );
