8613300c8685dc85e6cc1ee3d9ac4cf8e19371ff
Kasia Gawlak <kasia@paper-leaf.com>
2024-11-05 16:07:34 -0700

    removed bootstrap accordion and bootstrap javascript due to conflict with nav menu; replaced with accordion-js

---
+++ src/js/accordion.js
@@ -0,0 +1,7 @@
+import Accordion from 'accordion-js';
+
+const accordions = Array.from(document.querySelectorAll('.accordion'));
+new Accordion(accordions, {
+	duration: 250,
+	openOnInit: [0],
+});
